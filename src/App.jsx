0e0ef33b64331fd00122568bcc708c4e9281033d
Mamad78 <mamadi78.94@gmail.com>
2025-10-24 23:50:51 +0330

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404";
+
 function App() {
   return (
-    <>
-      <h1>shop project</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/products" />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
