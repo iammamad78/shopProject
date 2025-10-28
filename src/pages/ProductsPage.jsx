5e88834e2f651ff76c8b67bd52ed02ead467cc96
Mamad78 <mamadi78.94@gmail.com>
2025-10-29 01:26:41 +0330

    SearchInput added

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,22 +1,43 @@
 import Loader from "../components/Loader";
 import Cart from "../components/Cart";
+
 import { useProducts } from "../context/ProductsContext";
 
 import styles from "./ProductsPage.module.css";
+import { ImSearch } from "react-icons/im";
+import { useState } from "react";
 
 function ProductsPage() {
+  const [search, setSearch] = useState("");
   const products = useProducts();
 
+  const searchHandler = () => {
+    console.log("Searching for:", search);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Cart key={product.id} product={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>SideBar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Cart key={product.id} product={product} />
+          ))}
+        </div>
+        <div>SideBar</div>
+      </div>
+    </>
   );
 }
 
