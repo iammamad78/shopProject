1dee01f78732fbe2679e2acf7eb339da82b9b901
Mamad78 <mamadi78.94@gmail.com>
2025-10-29 01:45:01 +0330

    categories added

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -5,18 +5,34 @@ import { useProducts } from "../context/ProductsContext";
 
 import styles from "./ProductsPage.module.css";
 import { ImSearch } from "react-icons/im";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FaListUl } from "react-icons/fa";
 
 function ProductsPage() {
-  const [search, setSearch] = useState("");
   const products = useProducts();
+  const [search, setSearch] = useState("");
+  const [displayed, setDisplayed] = useState([]);
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
 
   const searchHandler = () => {
     console.log("Searching for:", search);
   };
 
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+
+    console.log(category);
+
+    if (tagName !== "LI") return;
+  };
+
   return (
     <>
+      {/* Search Input */}
       <div>
         <input
           type="text"
@@ -28,14 +44,30 @@ function ProductsPage() {
           <ImSearch />
         </button>
       </div>
+
       <div className={styles.container}>
+        {/* Main Section */}
         <div className={styles.products}>
-          {!products.length && <Loader />}
-          {products.map((product) => (
+          {!displayed.length && <Loader />}
+          {displayed.map((product) => (
             <Cart key={product.id} product={product} />
           ))}
         </div>
-        <div>SideBar</div>
+
+        {/* Sidebar */}
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+            <ul onClick={categoryHandler}>
+              <li>All</li>
+              <li>Electronics</li>
+              <li>Jewelery</li>
+              <li>Men's Clothing</li>
+              <li>Women`s Clothing</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
