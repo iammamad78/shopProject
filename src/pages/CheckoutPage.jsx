145fc2d3158a575ae6a385ab4ad9665784edfde3
Mamad78 <mamadi78.94@gmail.com>
2025-11-16 01:11:30 +0330

    CheckoutPage & BasketSidebar added

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,8 @@
 import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
 import { useCart } from "../context/CartContext";
+
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
@@ -8,9 +11,19 @@ function CheckoutPage() {
     dispatch({ type, payload });
   };
 
+  if (!state.itemsCounter)
+    return (
+      <div className={styles.container}>
+        <p>No Items Added to Card</p>
+      </div>
+    );
+
   return (
-    <div>
+    <div className={styles.container}>
       <div>
+        <BasketSidebar state={state} clickHandler={clickHandler} />
+      </div>
+      <div className={styles.products}>
         {state.selectedProducts.map((product) => (
           <BasketCard
             key={product.id}
